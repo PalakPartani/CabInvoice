2ec907f554db37162cade6a8eaeead21cb9baae8
PalakPartani <partanipalak16@gmail.com>
2020-05-14 00:00:54 -0700

    ADDED:feature to calculate fare for multiple rides

---
+++ src/main/java/com/cabinvoice/Ride.java
@@ -0,0 +1,12 @@
+package com.cabinvoice;
+
+public class Ride {
+    public double distance;
+    public double time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+
+    }
+}
---
+++ src/test/java/com/cabinvoice/CabInvoiceTest.java
@@ -0,0 +1,32 @@
+package com.cabinvoice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnCorrectTotalFare() {
+        CabInvoice invoiceService = new CabInvoice();
+        double fare = invoiceService.calculateFare(2.0, 5);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenMinDistanceAndTime_ShouldReturnCorrectTotalFare() {
+        CabInvoice invoiceService = new CabInvoice();
+        double fare = invoiceService.calculateFare(0.5, 3);
+        Assert.assertEquals(8, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeForMultipleRides_ShouldReturnCorrectTotalFare() {
+        CabInvoice invoiceService = new CabInvoice();
+        Ride[] ride = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+
+        double fare = invoiceService.calculateFare(ride);
+        Assert.assertEquals(30, fare, 0.0);
+    }
+}
